ae7edc6a3da27241d07a3866a9f57e92cff73066
Joseph Hui <joseph.hui@livestream.com>
2016-11-23 12:41:06 -0500

    invert the signs in _sortNewestFirst test

--- test.js
+++ test.js
@@ -103,20 +103,20 @@ describe('gce-images', function () {
 
   describe('_sortNewestFirst', function () {
     describe('when imageA.creationTimestamp is newer than imageB.creationTimestamp', function () {
-      it('should return 1', function () {
+      it('should return -1', function () {
         var imageA = { creationTimestamp: '2016-10-22T13:06:19.143-08:00' }
         var imageB = { creationTimestamp: '2016-09-25T07:31:52.339-07:00' }
 
-        assert.strictEqual(gceImages._sortNewestFirst(imageA, imageB), 1)
+        assert.strictEqual(gceImages._sortNewestFirst(imageA, imageB), -1)
       })
     })
 
     describe('when imageA.creationTimestamp is older than imageB.creationTimestamp', function () {
-      it('should return -1', function () {
+      it('should return 1', function () {
         var imageA = { creationTimestamp: '2016-10-22T13:06:19.143-08:00' }
         var imageB = { creationTimestamp: '2016-11-17T14:37:55.828-08:00' }
 
-        assert.strictEqual(gceImages._sortNewestFirst(imageA, imageB), -1)
+        assert.strictEqual(gceImages._sortNewestFirst(imageA, imageB), 1)
       })
     })
 
