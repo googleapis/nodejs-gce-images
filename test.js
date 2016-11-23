1f2b371e0844a8c71ba541a9cc6b2c1877de0b3c
Stephen Sawchuk <sawchuk@gmail.com>
2016-11-23 15:49:48 -0500

    code style

--- test.js
+++ test.js
@@ -108,8 +108,8 @@ describe('gce-images', function () {
         var imageB = { creationTimestamp: '2016-09-25T07:31:52.339-07:00' };
 
         assert.strictEqual(gceImages._sortNewestFirst(imageA, imageB), -1);
-      })
-    })
+      });
+    });
 
     describe('when imageA.creationTimestamp is older than imageB.creationTimestamp', function () {
       it('should return 1', function () {
@@ -117,8 +117,8 @@ describe('gce-images', function () {
         var imageB = { creationTimestamp: '2016-11-17T14:37:55.828-08:00' };
 
         assert.strictEqual(gceImages._sortNewestFirst(imageA, imageB), 1);
-      })
-    })
+      });
+    });
 
     describe('when imageA.creationTimestamp is equal to imageB.creationTimestamp', function () {
       it('should return 0', function () {
@@ -126,7 +126,7 @@ describe('gce-images', function () {
         var imageB = { creationTimestamp: '2016-08-25T07:14:24.426-07:00' };
 
         assert.strictEqual(gceImages._sortNewestFirst(imageA, imageB), 0);
-      })
-    })
-  })
+      });
+    });
+  });
 });
