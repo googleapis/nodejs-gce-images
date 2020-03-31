a71a39a5f4a781f1cb34daa6098582296073704f
bcoe <bencoe@google.com>
2020-03-31 16:26:19 -0700

    chore: address linting issues

--- test/test.ts
+++ test/test.ts
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {describe, it} from 'mocha';
 import * as assert from 'assert';
 import {GCEImages, Image} from '../src';
 
