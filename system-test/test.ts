df0728732054ad79c6ea2ac57a4eb0aec063c0dd
Steffany Brown <30247553+steffnay@users.noreply.github.com>
2020-04-01 14:20:37 -0700

    build!: update to latest version of gts/typescript (#223)
    * feat: update to latest version of gts/typescript
    
    Co-authored-by: bcoe <bencoe@google.com>

--- system-test/test.ts
+++ system-test/test.ts
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {describe, it, before} from 'mocha';
 import {assert} from 'chai';
 import {GCEImages, Image, ImageMap, ImagesMap} from '../src';
 
