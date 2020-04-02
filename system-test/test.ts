85c266c7bc4a41523d5144dd16de23d8ab669ce7
yoshi-automation <yoshi-automation@google.com>
2020-04-02 04:09:15 -0700

    feat!: drop Node 8 from engines field (#221)
    https://github.com/googleapis/nodejs-gce-images/commit/66f0265bddeb93d8c612b70d0d3cf386f5ce56b2
    commit 66f0265bddeb93d8c612b70d0d3cf386f5ce56b2
    Author: Steffany Brown <30247553+steffnay@users.noreply.github.com>
    Date:   Tue Mar 31 12:28:21 2020 -0700
    
        feat!: drop Node 8 from engines field (#221)
    
        Drops Node 8 from the engines field.

--- system-test/test.ts
+++ system-test/test.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {describe, it, before} from 'mocha';
 import {assert} from 'chai';
 import {GCEImages, Image, ImageMap, ImagesMap} from '../src';
 
--- test/test.ts
+++ test/test.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {describe, it} from 'mocha';
 import * as assert from 'assert';
 import {GCEImages, Image} from '../src';
 
